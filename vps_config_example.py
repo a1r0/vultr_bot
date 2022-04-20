04f845ac262530cc4718e764053a79cce71b5b6b
Alexey Popov <a1r0@users.noreply.github.com>
2022-04-21 00:56:12 +0300

    add vps example config

---
+++ vps_config_example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+This module with API constants
+I'ts for example only
+VULTR_KEY - api key which you can find in hosting panel preferences
+TELEGRAM_API_KEY - api key which you can get from telegram bot father
+INSTANCE_ID - temporary key , which helps me to get data only from specific instance on VULTR HOSTING
+"""
+api_keys = {
+    "VULTR_KEY":"",
+    "TELEGRAM_API_KEY":"",
+    "INSTANCE_ID":""
+    }
