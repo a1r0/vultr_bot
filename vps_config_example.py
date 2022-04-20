17358651b63d563e9c60ad21dc68c905f90d5950
Alexey Popov <a1r0@users.noreply.github.com>
2022-04-21 00:56:53 +0300

    update vps config example

--- vps_config_example.py
+++ vps_config_example.py
@@ -6,6 +6,8 @@ I'ts for example only
 VULTR_KEY - api key which you can find in hosting panel preferences
 TELEGRAM_API_KEY - api key which you can get from telegram bot father
 INSTANCE_ID - temporary key , which helps me to get data only from specific instance on VULTR HOSTING
+
+Rename it in the regular vps_congig.py
 """
 api_keys = {
     "VULTR_KEY":"",
