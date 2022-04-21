d044dd89046ccbb47bb3134bd298d6e902d623f7
Alexey Popov <a1r0@users.noreply.github.com>
2022-04-21 22:20:44 +0300

    update instance service methpds

--- plugins/instance_service.py
+++ plugins/instance_service.py
@@ -2,40 +2,49 @@ import json
 import requests
 import vps_config as cfg
 
-URL = 'https://api.vultr.com/v2/instances/'
+URL = 'https://api.vultr.com/v2/instances'
 headers = {'Authorization': 'Bearer {}'.format(cfg.api_keys['VULTR_KEY']),
             'Content-Type': 'application/json'}
 class Instance:
 
     @staticmethod
-    def list_instances() -> dict:
+    def list_instances() -> list:
         response = requests.get(URL, headers=headers)
         data = json.loads(response.text)
-        for instances in data['instances']:
+        instances = []
+        for instance in data['instances']:
             instance = {
-                'id': instances['id'],
-                'label':instances['label'],
-                'os':instances['os'],
-                'ram':instances['ram'],
-                'disk':instances['disk'],
-                'main_ip':instances['main_ip'],
-                'vcpu_count':instances['vcpu_count'],
-                'region':instances['region'],
-                'server_status':instances['server_status'],
-                'power_status':instances['power_status']
+                'id': instance['id'],
+                'label':instance['label'],
+                'os':instance['os'],
+                'ram':instance['ram'],
+                'disk':instance['disk'],
+                'main_ip':instance['main_ip'],
+                'vcpu_count':instance['vcpu_count'],
+                'region':instance['region'],
+                'server_status':instance['server_status'],
+                'power_status':instance['power_status']
             }
-            return instance
+            instances.append(instance)
+        return instances
 
     @staticmethod
     def get_instance_bandwidth() -> dict:
-        response = requests.get(URL + '{}/bandwidth'.format(cfg.api_keys['INSTANCE_ID']),headers)
+        response = requests.get(URL + '{}/bandwidth'.format(cfg.api_keys['INSTANCE_ID']),headers=headers)
         data = json.loads(response.text)
-        for bandwidth in data['bandwidth']:
-            bandwidth = {
+        for result in data['bandwidth']:
+            result = {
                 'server_date': {
-                    'incoming_bytes':bandwidth['incoming_bytes'],
-                    'outgoing_bytes':bandwidth['outgoing_bytes']
+                    'incoming_bytes':result['incoming_bytes'],
+                    'outgoing_bytes':result['outgoing_bytes']
                 }
             }
         print(data)
-        return bandwidth
+        return result
+
+# TODO: Instance info
+    @staticmethod
+    def get_instance_info(instance_id) -> dict:
+        response = requests.get(URL + '/{}'.format(instance_id), headers=headers)
+        data = json.loads(response.text)
+        return data
