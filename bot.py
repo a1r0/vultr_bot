b5827bbf4ced814c54b603677cd474e2c2c8dc8e
Alexey Popov <a1r0@users.noreply.github.com>
2022-04-22 21:04:43 +0300

    cosmetic fix

--- bot.py
+++ bot.py
@@ -187,19 +187,7 @@ def main() -> None:
     dispatcher.add_handler(conv_handler)
 
     # Start the Bot
-<<<<<<< HEAD
-    # updater.start_polling()
-    updater.start_webhook(
-        listen='0.0.0.0',
-        port=8443,
-        url_path='TOKEN',
-        key='private.key',
-        cert='cert.pem',
-        webhook_url='https://rdbl.xyz:8443/TOKEN'
-        )
-=======
     updater.start_polling()
->>>>>>> parent of b3314ab (add webhook options)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
