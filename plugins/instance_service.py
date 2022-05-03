645ff1a3574b840b4e992a76db0347bfccd68654
Alexey Popov <a1r0@users.noreply.github.com>
2022-05-03 15:29:42 +0300

    Document instance service methods

--- plugins/instance_service.py
+++ plugins/instance_service.py
@@ -1,19 +1,23 @@
 import json
 import requests
 import vps_config as cfg
-""" This module has an methods which helps to fetch data from api """
 
+# This module has an methods which helps to fetch data from api
+
+
+#Static URL
 URL = 'https://api.vultr.com/v2/'
 headers = {'Authorization': 'Bearer {}'.format(cfg.api_keys['VULTR_KEY']),
             'Content-Type': 'application/json'}
 class Instance:
+    """ Class describes VPS instance properties and methods """
     def __init__(self,id,label):
         self.id = id
         self.label = label
 
-
     @staticmethod
     def list_instances() -> list:
+        """ Lists available instances """
         response = requests.get(URL +'instances', headers=headers)
         data = json.loads(response.text)
         instances = []
@@ -35,16 +39,17 @@ class Instance:
 
     @staticmethod
     def get_instance_bandwidth(instance_id):
+        """ Returns instance bandwidth  """
         response = requests.get(URL + f'instances/{instance_id}/bandwidth',headers=headers)
         bandwidth = json.loads(response.text)
         return bandwidth
 
 # TODO: Make human readable text
     def get_instance_info(self , instance_id):
+        """ Returns non-formated instance properties """
         response = requests.get(URL + f'instances/{instance_id}', headers=headers)
         data = json.loads(response.text)
         # formatted_data = json.dumps(data.get('instance'), indent=2 ,sort_keys=True)
         # text = 'Here is instance properties \n' + formatted_data
         # newtext = text.replace('"', ' ' , len(text))
         return data
-        
